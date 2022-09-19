01d62d15e0bf91ebba704656a73eb2302eabc39b
johnemmanuelb <johnemmanuel.bacalla@gmail.com>
2022-09-20 00:59:47 +0800

    Write two concrete fighter classes and play battle in main class

--- src/main/java/com/pandenutella/yt/YosokuTatakai.java
+++ src/main/java/com/pandenutella/yt/YosokuTatakai.java
@@ -1,6 +1,29 @@
 package com.pandenutella.yt;
 
+import com.pandenutella.yt.core.Battle;
+import com.pandenutella.yt.core.CharacterMoveExecutor;
+import com.pandenutella.yt.core.Clock;
+import com.pandenutella.yt.core.GameCharacter;
+import com.pandenutella.yt.core.Printer;
+import com.pandenutella.yt.core.clocks.ActualClock;
+import com.pandenutella.yt.core.exceptions.AllowedRepeatedMovesExceededException;
+import com.pandenutella.yt.core.exceptions.InvalidNumberOfMovesInComboException;
+import com.pandenutella.yt.core.printers.ActualPrinter;
+import com.pandenutella.yt.custom.characters.SampleFighterOne;
+import com.pandenutella.yt.custom.characters.SampleFighterTwo;
+
 public class YosokuTatakai {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidNumberOfMovesInComboException, AllowedRepeatedMovesExceededException {
+        GameCharacter character1 = new SampleFighterOne();
+        GameCharacter character2 = new SampleFighterTwo();
+
+        CharacterMoveExecutor executor1 = new CharacterMoveExecutor(character2);
+        CharacterMoveExecutor executor2 = new CharacterMoveExecutor(character1);
+
+        Printer printer = new ActualPrinter();
+        Clock clock = new ActualClock();
+
+        Battle battle = new Battle(character1, character2, executor1, executor2, printer, clock);
+        battle.fight();
     }
 }
---
+++ src/main/java/com/pandenutella/yt/custom/characters/SampleFighterTwo.java
@@ -0,0 +1,59 @@
+package com.pandenutella.yt.custom.characters;
+
+import com.pandenutella.yt.core.Combo;
+import com.pandenutella.yt.core.characters.Fighter;
+import com.pandenutella.yt.core.moves.Block;
+import com.pandenutella.yt.core.moves.Jab;
+import com.pandenutella.yt.core.moves.Straight;
+
+import java.util.Random;
+
+import static java.util.Arrays.asList;
+
+public class SampleFighterTwo extends Fighter {
+
+    private final Random random = new Random();
+
+    @Override
+    public String getName() {
+        return "TWFI";
+    }
+
+    @Override
+    public Combo initiateAttack() {
+        int comboIndicator = random.nextInt(6);
+
+        return getAttackComboByIndicator(comboIndicator);
+    }
+
+    @Override
+    public Combo formulateCounter() {
+        int comboIndicator = random.nextInt(6);
+
+        return getCounterComboByIndicator(comboIndicator);
+    }
+
+    private Combo getAttackComboByIndicator(int indicator) {
+        return switch (indicator) {
+            case 0 -> new Combo(asList(new Jab(), new Jab(), new Straight()));
+            case 1 -> new Combo(asList(new Jab(), new Straight(), new Jab()));
+            case 2 -> new Combo(asList(new Straight(), new Jab(), new Jab()));
+            case 3 -> new Combo(asList(new Straight(), new Straight(), new Jab()));
+            case 4 -> new Combo(asList(new Straight(), new Jab(), new Straight()));
+            case 5 -> new Combo(asList(new Jab(), new Straight(), new Straight()));
+            default -> null;
+        };
+    }
+
+    private Combo getCounterComboByIndicator(int indicator) {
+        return switch (indicator) {
+            case 0 -> new Combo(asList(new Block(), new Jab(), new Jab()));
+            case 1 -> new Combo(asList(new Jab(), new Block(), new Jab()));
+            case 2 -> new Combo(asList(new Jab(), new Jab(), new Block()));
+            case 3 -> new Combo(asList(new Jab(), new Block(), new Block()));
+            case 4 -> new Combo(asList(new Block(), new Jab(), new Block()));
+            case 5 -> new Combo(asList(new Block(), new Block(), new Jab()));
+            default -> null;
+        };
+    }
+}
